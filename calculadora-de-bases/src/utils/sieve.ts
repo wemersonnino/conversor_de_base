d64c02bc93e0ba0f23047a2db0cf818539d62c2e
Wemerson Nino <wemersonnino@gmail.com>
2025-06-04 10:03:06 -0300

    Fix sieve algorithm to include limit

--- calculadora-de-bases/src/utils/sieve.ts
+++ calculadora-de-bases/src/utils/sieve.ts
@@ -1,19 +1,23 @@
-export const sieveOfEratosthenes = (n: number): { steps: string[], primes: number[] } => {
+export const sieveOfEratosthenes = (
+    n: number,
+): { steps: string[]; primes: number[] } => {
     const steps: string[] = [];
-    const primes = Array(n).fill(true);
+    const primes = Array(n + 1).fill(true);
     primes[0] = primes[1] = false;
 
-    for (let i = 2; i < Math.sqrt(n); i++) {
+    for (let i = 2; i * i <= n; i++) {
         if (primes[i]) {
             steps.push(`Número primo encontrado: ${i}`);
-            for (let j = i * i; j < n; j += i) {
+            for (let j = i * i; j <= n; j += i) {
                 primes[j] = false;
                 steps.push(`Marcando múltiplo ${j} de ${i} como não primo`);
             }
         }
     }
 
-    const primeNumbers = primes.map((prime, idx) => prime ? idx : null).filter(Number) as number[];
+    const primeNumbers = primes
+        .map((prime, idx) => (prime ? idx : null))
+        .filter((v): v is number => v !== null);
     steps.push(`Primos encontrados: ${primeNumbers.join(", ")}`);
 
     return { steps, primes: primeNumbers };
